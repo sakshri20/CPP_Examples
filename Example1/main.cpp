17c0c54f33a6de81edbc9bbda5ac1a0718f9753f
sakshri20 <sak.20.shrivastava@gmail.com>
2017-08-22 11:12:44 -0400

    Repository created
    Repository created

---
+++ Example1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "boy \n I \n love \n bacon \n";
+    cout << "and \n ham \n";
+    return 0;
+}
---
+++ InputExample/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "Enter a number" << endl;
+    cin >> a;
+
+    cout << "Enter another number" << endl;
+    cin >> b;
+
+    int sum = a + b;
+
+    cout << "Sum of a & b is  " << sum << endl;
+
+    return 0;
+}
